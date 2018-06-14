1768ed2252fba1aa31c827b8ad0d614f3aed3d8b
Aubrey Ross <aubreyjean@airbears2-10-142-109-245.airbears2.1918.berkeley.edu>
2018-06-14 12:18:38 -0700

    message

---
+++ assistant.sh
@@ -0,0 +1,61 @@
+echo "Hi Aubrey, I'm Helpful Helperton! I'm here to assist on your coding adventures!"
+
+printf "\n"
+
+date
+
+printf "\n"
+
+cal
+
+printf "\n"
+
+echo "Here are some helpful tips to help you on your journey!"
+
+printf "\n"
+
+echo "Tip 1. Type jupyter notebook to open a jupyter notebook."
+
+printf "\n"
+ 
+echo "Tip 2. Type screencapture filename.png to take a screenshot."
+
+printf "\n"
+ 
+echo "Tip 3. Type open filename.ext to open it."
+
+printf "\n"
+ 
+echo "Tip 4. Type touch to create a file."
+
+printf "\n"
+ 
+echo "Tip 5. Type cp to copy and paste a file."
+
+printf "\n"
+ 
+echo "Tip 6. Type rm to delete a file."
+
+printf "\n"
+ 
+echo "Tip 7. Type whoami to show the current user."
+
+printf "\n"
+ 
+echo "Tip 8. Type mkdir to create a directory."
+
+printf "\n"
+ 
+echo "Tip 9. Type ls to list the files in the working directory."
+
+printf "\n"
+ 
+echo "Tip 10. Type cd to change directory."
+
+printf "\n"
+ 
+echo "Bonus Tip: Take a deep breath! you GOT THIS!"
+
+printf "\n"
+ 
+
